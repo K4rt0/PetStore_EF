79b71dfc24a4ff5cdd8b136cc0b55fa1e9dfc348
K4rt0 <duongkhoanam147@gmail.com>
2023-10-09 22:42:58 +0700

    Update Base

---
+++ PetStore_EF/ViewModel/MainVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PetStore_EF.ViewModel
+{
+	public class MainVM : BaseVM
+	{
+		public MainVM()
+		{
+			MessageBox.Show("Alo");
+		}
+	}
+}
